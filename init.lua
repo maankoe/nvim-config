c5c8134f005be2768f6e2ccaf0408ba50db627d5
maankoe <maankoe@bernard>
2026-06-04 23:06:11 +0200

    highlight on yank and bottom terminal

--- init.lua
+++ init.lua
@@ -7,3 +7,18 @@ vim.opt.relativenumber = true
 
 require("plugins.lazy")
 require("lsp_config")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.keymap.set("n", "<leader>st", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 15)
+end)
